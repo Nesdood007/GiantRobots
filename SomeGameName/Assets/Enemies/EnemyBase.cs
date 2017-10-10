3c996de1ddf55a0f6f607e3105d833a211ac4644
Danielle <talleyad@email.sc.edu>
2017-10-10 18:26:03 -0400

    Added Scorpion Enemy

---
+++ SomeGameName/Assets/Enemies/EnemyBase.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class EnemyBase<T> 
+    where T : RegionBase
+{
+    public static Manager GameManager;
+    public static Vector3 Gravity;
+    public static System.Random Random;
+    System.Object lockGM = new System.Object();
+    System.Object lockGravity = new System.Object();
+    System.Object lockRandom = new System.Object();
+
+    public EnemyBase(int health, int damage, float speed, bool canWanderThroughRegions, float spawnRate, Rarity rarity)        
+    {
+        Health = health;
+        Damage = damage;
+        Speed = speed;
+        CanWanderThroughRegions = CanWanderThroughRegions;
+        IsAlive = true;
+        Rarity = rarity;
+        SpawnRate = spawnRate;
+
+        lock(lockGM)
+            GameManager = GameManager ?? GameObject.FindGameObjectWithTag("GameManager").GetComponent<Manager>();
+        lock(lockGravity)
+            Gravity = Vector3.down * 5f;
+        lock(lockRandom)
+            Random = Random ?? new System.Random();
+    }
+
+    public void OnUpdate()
+    {
+        if (GameManager.GameIsGoing && IsAlive)
+        {
+            ExecuteOnUpdate();
+        }
+    }
+
+    public bool IsAlive
+    {
+        get; set;
+    }
+
+    public int Health
+    {
+        get;
+        private set;
+    }
+
+    public int Damage
+    {
+        get;
+        private set;
+    }
+
+    public float Speed
+    {
+        get;
+        set;
+    }
+
+    public bool CanWanderThroughRegions
+    {
+        get;
+        private set;
+    }
+
+    public Effects DamageEffect
+    {
+        get;
+        protected set;
+    }
+
+    public List<Effects> Resistances
+    {
+        get;
+        protected set;
+    }
+
+    public float SpawnRate
+    {
+        get;
+        private set;
+    }
+
+    public Rarity Rarity
+    {
+        get;
+        private set;
+    }
+
+    public abstract void ExecuteOnUpdate();
+
+    public abstract void Move(CharacterController characterController, Transform transform);
+
+    public abstract void Attack();
+
+    public abstract void DropItem();
+
+}
---
+++ SomeGameName/Assets/Enemies/Scorpion/Scorpion.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class Scorpion : MonoBehaviour
+{
+    public int health = 10;
+    public int damage = 5;
+    public float speed = 5f;
+    public bool canWanderThroughRegions = false;
+    public float spawnRate = 5f;
+    bool oldWanderingBounds;
+    ScorpionObject scorpionObject;
+    CharacterController characterController;
+
+
+    void Awake()
+    {
+        scorpionObject = new ScorpionObject(health, damage, speed, canWanderThroughRegions, spawnRate);
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+       if(!oldWanderingBounds && scorpionObject.WanderingBoundsHasBeenSet)
+        {
+            scorpionObject.SetRandomDirection(transform);
+            scorpionObject.SetRandomStartingPosition(transform);
+        }
+        if (scorpionObject.WanderingBoundsHasBeenSet && scorpionObject.IsAlive && ScorpionObject.GameManager.GameIsGoing)
+        {
+            scorpionObject.Speed = speed;
+            scorpionObject.Move(characterController, transform);            
+        }        
+        oldWanderingBounds = scorpionObject.WanderingBoundsHasBeenSet;
+    }
+
+    public void SetEnemyWanderingBounds()
+    {
+        var regions = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Manager>().RegionPositions;
+        if (regions.Keys.Contains(scorpionObject.PrimaryRegion))
+        {
+            scorpionObject.wanderingBounds = regions[scorpionObject.PrimaryRegion];
+            scorpionObject.WanderingBoundsHasBeenSet = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player" || collider.gameObject.tag == "Enemy")
+        {
+            scorpionObject.SetRandomDirection(transform);
+            Debug.Log("?");
+        }
+    }
+
+}
+
+public class ScorpionObject : EnemyBase<Desert>
+{
+    public Vector3 direction;
+    public Rect wanderingBounds;
+    public bool WanderingBoundsHasBeenSet = false;
+    public Regions PrimaryRegion = Regions.Desert;    
+
+    public ScorpionObject(int health, int damage, float speed, bool canWanderThroughRegions, float spawnRate)
+        : base(health, damage, speed, canWanderThroughRegions, spawnRate, Rarity.Common)
+    {     
+        
+    }
+
+    public void SetRandomDirection()
+    {
+        direction = new Vector3((Random.Next() %2==0? -1 : 1) *(Random.Next() % 100), 0, (Random.Next() % 2 == 0 ? -1 : 1) * (Random.Next() % 100)).normalized;
+        //transform.rotation = Quaternion.LookRotation(transform.TransformDirection(direction));
+    }
+
+    public void SetRandomDirection(Transform transform)
+    {
+        direction = new Vector3((Random.Next() % 2 == 0 ? -1 : 1) * (Random.Next() % 100), 0, (Random.Next() % 2 == 0 ? -1 : 1) * (Random.Next() % 100)).normalized;
+        transform.rotation = Quaternion.LookRotation(transform.TransformDirection(direction));
+    }
+
+    public void SetRandomStartingPosition(Transform transform)
+    {
+        var startingPosition = new Vector3(Random.Next() % wanderingBounds.width + wanderingBounds.xMin, 0, Random.Next() % wanderingBounds.height + wanderingBounds.yMin);
+        transform.position = new Vector3(startingPosition.x, Terrain.activeTerrain.SampleHeight(startingPosition) + 2f, startingPosition.z);
+    }
+
+    public void SetEnemyWanderingBounds()
+    {
+        var regions = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Manager>().RegionPositions;
+        
+        if (regions.Keys.Contains(PrimaryRegion))
+        {
+            wanderingBounds = regions[PrimaryRegion];
+            WanderingBoundsHasBeenSet = true;
+            
+        }
+    }
+
+    internal Rect GetEnemyWanderingBounds()
+    {
+        return wanderingBounds;
+    }
+
+    public override void ExecuteOnUpdate()
+    {
+
+    }
+
+    public override void Move(CharacterController characterController, Transform transform)
+    {
+        
+        var newPos = transform.TransformDirection(-.5f, 0, -.5f);
+
+        newPos *= Speed * Time.deltaTime;
+        characterController.SimpleMove(newPos + Gravity);
+
+        var projectedPosition = characterController.velocity + transform.position;
+        if (projectedPosition.x < wanderingBounds.xMin || projectedPosition.x > wanderingBounds.xMax || projectedPosition.z < wanderingBounds.yMin || projectedPosition.z > wanderingBounds.yMax || TerrainModifier.Grasslands.PointIsInCircle(new Vector2(projectedPosition.x, projectedPosition.z)))
+        {
+            SetRandomDirection(transform);
+        }
+    }
+
+   
+    
+
+    public override void Attack()
+    {
+
+    }
+
+    public override void DropItem()
+    {
+
+    }
+}
